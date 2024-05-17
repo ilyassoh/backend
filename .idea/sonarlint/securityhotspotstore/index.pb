e1f64e411356beedd5f7e561044ae8fb5eb726cc
723malak <malak.elkiram@gmail.com>
2024-05-18 00:16:29 +0100

    backendM

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,7 @@
 
 Y
 )src/main/resources/application.properties,0\6\06f65155ec73600b9fc5c4dc2883d4bb84d8e284
+|
+Lsrc/main/java/com/phegondev/usersmanagementsystem/service/ClientService.java,c\9\c9c25ea7151528cb9148a2f508893cb60258f9d7
+}
+Msrc/main/java/com/phegondev/usersmanagementsystem/model/enumm/Type_Place.java,a\f\af382a67f51573e77a0aefc234073bd7e5f2ec3a
--- src/main/java/com/phegondev/usersmanagementsystem/repository/ReservationRep.java
+++ src/main/java/com/phegondev/usersmanagementsystem/repository/ReservationRep.java
@@ -1,9 +1,12 @@
 package com.phegondev.usersmanagementsystem.repository;
 
 import com.phegondev.usersmanagementsystem.model.ReservationModel;
+import com.phegondev.usersmanagementsystem.model.enumm.Status_Reservation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,9 @@ public interface ReservationRep extends JpaRepository<ReservationModel, Long> {
     List<ReservationModel> findAll();
     List<ReservationModel> findAllByClientId(Long clientId);
     Page<ReservationModel> findAllByClientId(Long clientId,Pageable p );
+    List<ReservationModel> findByPlaceId(Long placeId);
+    List<ReservationModel> findByParkingId(Long parkingId);
+    @Query("SELECT r FROM ReservationModel r WHERE r.place.id = :placeId AND r.status = :status")
+    List<ReservationModel> findByPlaceIdAndStatus(@Param("placeId") Long placeId, @Param("status") Status_Reservation status);
+    ReservationModel getReservationById(Long id);
 }
